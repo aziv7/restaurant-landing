e6ee7d2084e307e406f84923a0992bbfac788b63
aziv7 <57321183+aziv7@users.noreply.github.com>
2022-12-30 00:40:46 +0100

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,7 +3,15 @@ import React from 'react';
 const Contact = () => {
   return (
     <div className='section' id='contact'>
-      Contact
+      <div className='border-solid border-black border-b pb-8 flex justify-between items-end gap-4 flex-wrap '>
+        <div>
+          <div className='uppercase'>Lets talk</div>
+          <div className='text-3xl font-bold'>want to reserve a table?</div>
+        </div>
+        <div className='btn'>
+          <a href=''>Contact us</a>
+        </div>
+      </div>
     </div>
   );
 };
