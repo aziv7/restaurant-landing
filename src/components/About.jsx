d680aafe05f134207d075af17b71a86d85c5047d
aziv7 <57321183+aziv7@users.noreply.github.com>
2022-12-30 00:09:26 +0100

    about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,43 @@ import chef from '../assets/chef.jpg';
 const About = () => {
   return (
     <div className='section' id='about'>
-      About
+      <div className='grid md:grid-cols-2 items-center mb-10'>
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <div className='sm:text-3xl text-8xl font-bold mb-6'>
+            Best food in town awaits you
+          </div>
+          <p className='text-sm opacity-70'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+            laboriosam laudantium, temporibus aliquam fuga non molestias veniam
+            rerum aliquid a?
+          </p>
+          <div className='btn'>
+            <a href='' className='text-white text-[0.85rem]'>
+              Explore More
+            </a>
+            <FaArrowRight className='text-white ' />
+          </div>
+        </div>
+      </div>
+      <div className='md:row-start-1'>
+        <img src={pasta} alt='' />
+      </div>
+      <div className='grid md:grid-cols-2 gap-10 items-center'>
+        <div>
+          <div className='sm:text-3xl text-xl font-bold mb-6 '>
+            Our team of registred nurses and skilled healthcare professionals
+            provide in-home nursing.
+          </div>
+          <p className='text-sm opacity-70'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt ab
+            voluptatum iste. Ipsum perspiciatis nesciunt a reprehenderit? A,
+            dolorum dolorem.
+          </p>
+        </div>
+        <div>
+          <img src={chef} alt='' />
+        </div>
+      </div>
     </div>
   );
 };
